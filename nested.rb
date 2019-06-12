24e7f74af554cb9320e93821c33a4657dad21f55
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-12 21:22:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,10 +83,7 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = {
-			 :known_for => "Ruby",
-		   :languages => ["LISP", "C"]
-	 }
+  programmer_hash[:yukihiro_matsumoto] = "values"
 end
 
 def changing_alan
